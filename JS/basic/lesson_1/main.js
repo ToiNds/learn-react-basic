7706f1464b5d44d861e4beba2192ee739dd01566
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-08-17 15:36:29 +0700

    js basic

---
+++ JS/basic/lesson_1/main.js
@@ -0,0 +1,18 @@
+var fullName = "Nguyen Dinh Toi";
+var age = 20;
+
+alert("Xin chao: " + fullName + "\nTuoi: " + age);
+
+/**
+ * Giới thiệu 1 số hàm built-in
+ * 1. Alert
+ * 2. Confirm
+ * 3. Console
+ * 4.Prompt
+ * 5. setTimeout --> Chạy đoạn code sau 1 khoảng thời gian(Chỉ chạy 1 lần).
+ * 6. setInterval --> Chạy đoạn code sau 1 khoảng thời gian(Chạy liên tục).
+ */
+
+var age = 35;
+var nextAge = ++age;
+console.log(nextAge);
